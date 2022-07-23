2902e33e45c38375a7b5866ed6f1a40a3c6a4fc6
Sam Parker <s4m.prkr@gmail.com>
2022-07-23 14:48:21 +0100

    Implemented a simple Minimax algorithm

---
+++ minimaxer.py
@@ -0,0 +1,49 @@
+import enum as e
+import numpy as np
+
+class ResultType(e.Enum):
+
+    HEURISTIC = 0
+    LEAF_NODE = 1
+    NORMAL = 2
+
+class Result:
+    
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+        self.children = []
+
+    def updateValue(self, value):
+        self.value = value
+
+    def addChild(self, child):
+        self.children.append(child)
+
+class Minimaxer:
+
+    def __init__(self, node, maximising, limit=5):
+        self.limit = limit
+        self.tree = self.minimax(node=node, maximising=maximising)
+
+    def minimax(self, node, maximising, depth=0):
+        if depth >= self.limit:
+            return Result(ResultType.HEURISTIC, node.evaluate())
+        
+        children = node.getChildren()
+        if len(children) < 1:
+            return Result(ResultType.LEAF_NODE, node.evaluate())
+
+        initial = np.NINF if maximising else np.Inf
+        optimal = Result(ResultType.NORMAL, initial)
+
+        for child in children:
+            result = self.minimax(child, not maximising, depth=depth-1)
+            optimal.addChild(result)
+
+            if maximising and result.value > optimal.value:
+                optimal.updateValue(result.value)
+            elif not maximising and result.value < optimal.value:
+                optimal.updateValue(result.value)
+        
+        return optimal
