247587c49ed2da3f54b995cea29657028f8d88f4
Sam Parker <s4m.prkr@gmail.com>
2022-07-26 00:01:14 +0100

    Other minor changes

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from src import Minimaxer, Player
 from games import TicTacToeBoard
 
-game = TicTacToeBoard()
-game.set((0, 1), Player.MAX)
-game.set((1, 1), Player.MAX)
-game.set((2, 1), Player.MAX)
-print(game.evaluate())
+b = TicTacToeBoard()
+b.set((0, 0), Player.MAX)
+# b.set((1, 0), Player.MAX)
+# b.set((0, 1), Player.MIN)
+# b.set((1, 1), Player.MIN)
+# b.set((2, 1), Player.MAX)
+# b.set((1, 2), Player.MAX)
+# b.set((0, 2), Player.MIN)
+print(b)
+
+
+m = Minimaxer(b, Player.MIN, limit=5, use_transposition=False)
+print(m.tree)
+print(len(m.tree))
+
