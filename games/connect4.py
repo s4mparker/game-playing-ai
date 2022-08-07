d3e90dc45f7374d2addfaa18308182422a40673f
Sam Parker <s4m.prkr@gmail.com>
2022-08-07 23:17:21 +0100

    Implemented child function for Connect4

--- games/connect4.py
+++ games/connect4.py
@@ -10,10 +10,10 @@ class Connect4Board:
         self.board = np.full((height, width), None, dtype=Player)
 
     def __str__(self):
-        return '\n'.join([[' '.join([str(pos) if pos is not None else self.default for pos in row]) for row in self.board]])
+        return '\n'.join([' '.join([str(pos) if pos is not None else self.default for pos in row]) for row in self.board])
 
     def __hash__(self):
-        return 0
+        return int.from_bytes(str.encode(''.join(pos if pos is not None else self.default for pos in self.board.flatten())), byteorder='little')
 
     def set(self, coordinates, player):
         (x, y) = coordinates
@@ -32,7 +32,22 @@ class Connect4Board:
     def evaluate(self):
         pass
 
-    def children(self):
-        pass
+    def children(self, player):
+        children = []
+        for column in range(self.board.shape[1]):
+            for row in range(self.board.shape[0]):
+                if self.get((column, row)) is not None:
+                    if row > 0:
+                        child = self.copy()
+                        child.set((column, row-1), player)
+                        children.append(child)
+                    break
+                elif row == self.board.shape[0] - 1:
+                    child = self.copy()
+                    child.set((column, row), player)
+                    children.append(child)
+                    break
+
+        return children
 
 
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from utils import Player, dfs, bfs
 from minimax import Minimax
-from games import TicTacToeBoard
+from games import TicTacToeBoard, Connect4Board
 
-board = TicTacToeBoard()
-print(board)
+# board = TicTacToeBoard()
+# print(board)
 
-m = Minimax.generate(board, Player.MAX, transpositions=False, alpha_beta=False, limit=2)
+# m = Minimax.generate(board, Player.MAX, transpositions=False, alpha_beta=False, limit=2)
 
-func = lambda node : print(f'{" "*node.depth}{node}')
-bfs(m, func)
-print(f'Tree: {m.size}')
+# func = lambda node : print(f'{" "*node.depth}{node}')
+# bfs(m, func)
+# print(f'Tree: {m.size}')
+
+c = Connect4Board()
+print(c, end='\n\n')
+
+
+for child in c.children(Player.MAX):
+    print(child, end='\n\n')
